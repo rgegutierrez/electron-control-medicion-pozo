c0c54eb6b51676d281bd0f3e1d4f3080df9942f9
Jorge Gutierrez Salgado <jgutierrez@fpc.cl>
2024-02-18 17:54:34 -0300

    ajuste layout

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,56 @@
 <template>
   <v-app>
-    <NavigationBar />
-    <v-main>
-      <router-view></router-view> <!-- Este componente renderiza la vista de la ruta actual -->
-    </v-main>
+    <v-layout>
+      <v-navigation-drawer class="bg-deep-purple" theme="dark" app permanent>
+        <v-list>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="title text-center my-8"
+                >Control Medición Pozo</v-list-item-title
+              >
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item
+            to="/"
+            title="Mediciones"
+            prepend-icon="mdi-view-dashboard"
+          >
+          </v-list-item>
+          <v-list-item
+            to="/dashboard"
+            title="Dashboard"
+            prepend-icon="mdi-view-dashboard"
+          >
+          </v-list-item>
+          <v-list-item
+            to="/carga-datos"
+            title="Carga Archivo"
+            prepend-icon="mdi-view-dashboard"
+          >
+          </v-list-item>
+          <v-list-item
+            to="/pozo"
+            title="Pozos"
+            prepend-icon="mdi-view-dashboard"
+          >
+          </v-list-item>
+        </v-list>
+
+        <template v-slot:append>
+          <div class="pa-2">
+            <v-btn block> Logout </v-btn>
+          </div>
+        </template>
+      </v-navigation-drawer>
+      <v-main>
+        <router-view></router-view>
+      </v-main>
+    </v-layout>
   </v-app>
 </template>
 
 <script>
-import NavigationBar from './components/NavigationBar.vue';
-
 export default {
-  name: 'App',
-  components: {
-    NavigationBar,
-  },
+  name: "App",
 };
 </script>
